e593df493f8da504ddaf9b64971c97f182059a7d
Unai <unai.perez@familiados.com>
2022-04-23 15:30:53 +0200

    HomePage casi acabada

--- front/homePage.js
+++ front/homePage.js
@@ -1,9 +1,18 @@
 window.onload = () => {
 
     let cookiesModal = document.querySelector('#cookiesModal');
+    let rejectModal = document.querySelector('#rejectModal');
     let modal = document.querySelector('.cookiesModal');
 
     cookiesModal.addEventListener('click', function(){
+        modal.classList.add('right-5000');
+        setTimeout(() => {
+            modal.classList.remove('right-5000');
+            modal.classList.add('d-none');
+        }, 2000);
+    });
+
+    rejectModal.addEventListener('click', function(){
         modal.classList.add('right-5000');
         setTimeout(() => {
             modal.classList.remove('right-5000');
